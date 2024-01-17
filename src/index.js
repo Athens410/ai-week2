4dd85e23a625cbe3959edcca1e182fc28afa3595
Athens410 <athenah.coucean@gmail.com>
2024-01-17 10:59:38 -0500

    feat: Add event listener for poem form submission
    - Used document.querySelector to select the poem form element
    - Created a 'generatePoem' function to handle form submission
    - Added an event listener to the poem form for the 'submit' event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-let poemFormElement = document.querySelector("#poem-generator-form")
+
+function generatePoem(event){
+
+
+    event.preventDefualt()
+
+
+    alert("hello skies!")
+}
+
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+
+poemFormElement.addEventListener("submit", generatePoem);

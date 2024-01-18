12d9e2e23178a6316cfcda47e45aac9245464229
Athens410 <athenah.coucean@gmail.com>
2024-01-18 12:37:08 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "06a4f404921b3294b64d64f35o0f753t";
 let prompt = `Generate a French poem about ${instructionsInput.value}`;
 
-let context = "you are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. ";
+let context = "you are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions  ";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
